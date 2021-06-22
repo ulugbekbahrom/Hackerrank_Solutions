44eccca8ec1f267601b1861837955ded14038a4a
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-06-22 22:52:21 +0500

    Day 5, 6, 7 Solved

---
+++ DaysOfCode30/Day6/Solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        String str;
+
+        for(int i = 0; i < number; i++){
+            str = scanner.next();
+
+            for(int j = 0; j < str.length(); j+=2){
+
+                System.out.print(str.charAt(j));
+
+            }
+
+            System.out.print(" ");
+
+            for(int j = 1; j < str.length(); j+=2){
+
+                System.out.print(str.charAt(j));
+
+            }
+            System.out.println();
+        }
+    }
+}
