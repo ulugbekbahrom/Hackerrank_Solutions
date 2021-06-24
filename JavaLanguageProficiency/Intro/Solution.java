c0f2777e19c1d52355f053578985ff8b80c8e29f
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-06-24 23:52:56 +0500

    started Java Language Proficiency

---
+++ JavaLanguageProficiency/Intro/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.print("Hello, World.\n");
+        System.out.print("Hello, Java.");
+    }
+}
