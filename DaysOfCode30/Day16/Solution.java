e718e4b109a1a404426fc499a7e1f58350ed5160
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-06-24 23:43:06 +0500

    Day 16 Solved

---
+++ DaysOfCode30/Day16/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String S = bufferedReader.readLine();
+            System.out.println(Integer.parseInt(S));
+        } catch(NumberFormatException e){
+            System.out.println("Bad String");
+        }
+        bufferedReader.close();
+    }
+}
