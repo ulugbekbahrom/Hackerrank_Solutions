9a9314c5540dfc42afdb20fe5c1c31316af9aa8f
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-06-25 22:43:22 +0500

    done java output format

--- JavaLanguageProficiency/Intro/java-output-format/Solution.java
+++ JavaLanguageProficiency/Intro/java-output-format/Solution.java
@@ -5,13 +5,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("================================");
-        for(int i = 0; i < 3; i++)
-        {
+        for(int i = 0; i < 3; i++) {
             String str = sc.next();
             int x = sc.nextInt();
             System.out.printf("%-15s%03d\n", str, x);
         }
         System.out.println("================================");
-
     }
 }
