99f9cc97ca2a1eb88e7a75401ea5050ea85e9a31
Ulugbek Bakhrom <ulugxodja@gmail.com>
2021-06-27 21:29:19 +0500

    done java end-of-file

---
+++ JavaLanguageProficiency/Intro/java-end-of-file/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        while (scanner.hasNext()) {
+            i++;
+            System.out.println(i + " " + scanner.nextLine());
+        }
+    }
+}
