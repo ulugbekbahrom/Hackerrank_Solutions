8d644677998e7b86c1b75a41d0b7eed75dc06a9e
Ulugbek <ulugxodja@gmail.com>
2021-08-14 19:28:23 +0500

    done Java Anagrams

---
+++ JavaLanguageProficiency/Strings/Anagram/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static boolean isAnagram(String a, String b) {
+        // edge cases
+        if (a.length() != b.length() || a.equals("null") || b.equals("null")) {
+            return false;
+        }
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        int[] alphabet = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            alphabet[a.charAt(i) - 'a']++;
+        }
+
+        for (int j = 0; j < b.length(); j++) {
+            alphabet[b.charAt(j) - 'a']--;
+        }
+
+        for (int letter : alphabet) {
+            if (letter != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+    }
+}
